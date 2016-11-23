b2a8b85f1e143206ec7628ca3952d216c3cc7112
Francisco <azhagthott@gmail.com>
2016-11-23 19:21:04 -0300

    conectado fb

--- app/src/main/java/com/zecovery/android/ptrac/app/Calendario.java
+++ app/src/main/java/com/zecovery/android/ptrac/app/Calendario.java
@@ -6,6 +6,60 @@ package com.zecovery.android.ptrac.app;
 
 public class Calendario {
 
+    private String region;
+    private String comuna;
+    private String direccion;
+    private String telefono;
+private String estado;
 
+    public Calendario() {
+    }
 
+    public Calendario(String region, String comuna, String direccion, String telefono, String estado) {
+        this.region = region;
+        this.comuna = comuna;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.estado = estado;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getComuna() {
+        return comuna;
+    }
+
+    public void setComuna(String comuna) {
+        this.comuna = comuna;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
--- app/src/main/java/com/zecovery/android/ptrac/fragment/FragmentEst.java
+++ app/src/main/java/com/zecovery/android/ptrac/fragment/FragmentEst.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.firebase.crash.FirebaseCrash;
 import com.google.firebase.database.DataSnapshot;
@@ -17,13 +19,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.zecovery.android.ptrac.R;
+import com.zecovery.android.ptrac.app.Calendario;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.zecovery.android.ptrac.activity.BaseActivity.LOG_TAG;
 
 public class FragmentEst extends Fragment {
 
     private OnFragmentInteractionListener listener;
-    private WebView wvCalendar;
+    private Calendario calendario;
+    private LinearLayout container;
 
     public FragmentEst() {
         // Required empty public constructor
@@ -41,10 +48,10 @@ public class FragmentEst extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView =inflater.inflate(R.layout.fragment_est, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_est, container, false);
 
+        container = (LinearLayout) rootView.findViewById(R.id.container);
 
-        createCalendario();
 
         return rootView;
     }
@@ -70,42 +77,85 @@ public class FragmentEst extends Fragment {
     }
 
 
-    private void createCalendario(){
+    private List<Calendario> createCalendario() {
+
+        final List<Calendario> list = new ArrayList<>();
+
+        final Context context = getActivity().getApplicationContext();
 
         FirebaseDatabase db = FirebaseDatabase.getInstance();
         DatabaseReference reference = db.getReference();
 
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+        try {
+            reference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
 
+                    for (int i = 0; i < dataSnapshot.child("calendario").getChildrenCount(); i++) {
 
-                for (int i = 0; i <dataSnapshot.child("region").getChildrenCount(); i++) {
+                        String region = "sin datos";
+                        String comuna = "sin datos";
+                        String direccion = "sin datos";
+                        String telefono = "sin datos";
+                        String estado = "sin datos";
 
-                    Log.d(LOG_TAG, "i: " + i);
+                        if (equals(dataSnapshot.child("calendario").child("" + i + "").child("region").getValue())) {
+                            region = dataSnapshot.child("calendario").child("" + i + "").child("region").getValue().toString();
+                        }
 
-                    for (int j = 0; j <dataSnapshot.child("region").child(""+i+"").getChildrenCount() ; j++) {
-                        Log.d(LOG_TAG, "j: " + j);
+                        if (equals(dataSnapshot.child("calendario").child("" + i + "").child("comuna").getValue())) {
+                            comuna = dataSnapshot.child("calendario").child("" + i + "").child("comuna").getValue().toString();
+                        }
+
+                        if (equals(dataSnapshot.child("calendario").child("" + i + "").child("direccion").getValue())) {
+                            direccion = dataSnapshot.child("calendario").child("" + i + "").child("direccion").getValue().toString();
+                        }
+
+                        if (equals(dataSnapshot.child("calendario").child("" + i + "").child("telefono").getValue())) {
+                            telefono = dataSnapshot.child("calendario").child("" + i + "").child("telefono").getValue().toString();
+                        }
+
+                        if (equals(dataSnapshot.child("calendario").child("" + i + "").child("estado").getValue())) {
+                            estado = dataSnapshot.child("calendario").child("" + i + "").child("estado").getValue().toString();
+                        }
+
+                        calendario = new Calendario(region, comuna, direccion, telefono, estado);
+                        list.add(calendario);
+
+                        TextView textViewRegion = new TextView(context);
+                        TextView textViewComuan = new TextView(context);
+                        TextView textViewDireccion = new TextView(context);
+                        TextView textViewtelefono = new TextView(context);
+                        TextView textViewEstado = new TextView(context);
+
+                        textViewRegion.setText(region);
+                        textViewComuan.setText(comuna);
+                        textViewDireccion.setText(direccion);
+                        textViewtelefono.setText(telefono);
+                        textViewEstado.setText(estado);
+
+                        textViewRegion.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                        textViewComuan.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                        textViewDireccion.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                        textViewtelefono.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                        textViewEstado.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                        container.addView(textViewComuan);
+
                     }
-
-
-
                 }
 
-                //String comuna_key = dataSnapshot.child("region").child("4").child("comuna").child("5").child("comuna_nombre").getValue().toString();
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    FirebaseCrash.log("ERROR:: " + databaseError);
+                }
+            });
+        } catch (Exception e) {
+            Log.d(LOG_TAG, "Exception: " + e);
+        }
 
-                //Log.d(LOG_TAG, "comuna_key: " + comuna_key);
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                FirebaseCrash.log("ERROR:: " +databaseError);
-            }
-        });
-
+        return list;
     }
-
 
 
 }
